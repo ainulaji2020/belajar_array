061d232db9ab63fa387a56d60c23ac7ebb41fbaa
AinulAji <ainulaji163@gmail.com>
2021-01-10 23:23:45 +0700

    uppload projeck materi php

---
+++ belajar_array/array.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>belajar array</title>
+	</head>
+	<style>
+		
+
+	</style>
+	<body>
+		<?php  
+
+				$listmahasiswa = ["wahid abdulah", "elmobackhtiar" , "lendies fabri" ,"kendol"];
+
+				// echo "Nama saya adalah " .$listmahasiswa[1];
+
+				foreach ($listmahasiswa as $key => $value) {
+					echo  $value . "<br>";
+					// echo  $listmahasiswa[$key] . "<br>";
+				}
+
+
+
+		?>
+	</body>
+</html>
